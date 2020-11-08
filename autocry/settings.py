e2a4093428ac5efd5ee4f1746f16732ffb6ce49c
Takvor Garabedian <takvor@abv.bg>
2020-11-08 22:31:55 +0200

    change settings

--- autocry/settings.py
+++ autocry/settings.py
@@ -125,9 +125,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-]
+# location where django collect all static files
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# location where you will store your static files
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'autocry/static')]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
