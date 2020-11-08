cb5e38405968823d762da91c07b4188f0b165cd2
Takvor Garabedian <takvor@abv.bg>
2020-11-08 23:26:06 +0200

    trying heroku with sqlite

--- autocry/settings.py
+++ autocry/settings.py
@@ -75,12 +75,19 @@ WSGI_APPLICATION = 'autocry.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'autocry',
+#         'USER': 'postgres',
+#         'PASSWORD': 'admin'
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'autocry',
-        'USER': 'postgres',
-        'PASSWORD': 'admin'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
