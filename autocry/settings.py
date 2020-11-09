5de04b31fa863d894b2c137a66456dd9cc7d736e
Takvor Garabedian <takvor@abv.bg>
2020-11-09 19:03:08 +0200

    dropped heroku try

--- autocry/settings.py
+++ autocry/settings.py
@@ -25,8 +25,7 @@ SECRET_KEY = '=p#&$izoov^3=m-7%q-_k*^-)z55p)3f%j0+vr3$pw$y@t*kw='
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-# Heroku deployment
-ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1', 'autocry.herokuapp.com']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -76,19 +75,12 @@ WSGI_APPLICATION = 'autocry.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'autocry',
-#         'USER': 'postgres',
-#         'PASSWORD': 'admin'
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'autocry',
+        'USER': 'postgres',
+        'PASSWORD': 'admin'
     }
 }
 
@@ -129,7 +121,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'autocry/static')
+    os.path.join(BASE_DIR, 'static')
 ]
