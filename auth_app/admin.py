13846382cb9377472a3c5df7c7645189d8f8d93d
Takvor Garabedian <takvor@abv.bg>
2020-11-26 12:04:35 +0200

    tweaked user admin.py

--- auth_app/admin.py
+++ auth_app/admin.py
@@ -1,5 +1,33 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
 
 from auth_app.models import UserProfile
 
-admin.site.register(UserProfile)
+
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ('user',)
+
+
+class ProfileInline(admin.StackedInline):
+    model = UserProfile
+    can_delete = False
+    verbose_name_plural = 'UserProfile'
+    fk_name = 'user'
+
+
+class CustomUserAdmin(UserAdmin):
+    inlines = (ProfileInline,)
+
+    def get_inline_instances(self, request, obj=None):
+        if not obj:
+            return list()
+        return super(CustomUserAdmin, self).get_inline_instances(request, obj)
+
+
+# Changing the default name to user-friendly type
+admin.site.register(UserProfile, UserProfileAdmin)
+
+# Attaching the extended user profile to user change admin page
+admin.site.unregister(User)
+admin.site.register(User, CustomUserAdmin)
